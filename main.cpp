206d049ea129312898b44f1e0ec3ab83a13d1dc6
eduardo-lamounier <a2025952788@teiacoltec.org>
2025-08-27 15:59:00 -0300

    Adicionando o arquivo .cpp principal

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/* 
+Arquivo que ao ser compilado, vai dar origem ao executável do jogo
+*/
+
+#include<iostream>
+
+// =================================== main ===================================
+
+int main(int argc, char *argv[]){
+
+    
+    return 0;
+}
