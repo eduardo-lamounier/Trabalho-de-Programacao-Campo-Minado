df79b8d9a54b7e75d1aacd58b06100f6d3be4983
eduardo-lamounier <a2025952788@teiacoltec.org>
2025-08-28 19:11:03 -0300

    Criação do arquivo de teste para a implementação do header: config.hpp

---
+++ testing/configTest.cpp
@@ -0,0 +1,20 @@
+/*
+
+Criado por Eduardo
+Data de criação: 28/08/2025
+
+Arquivo de teste para o header config.hpp, tem como objetivo testar as funções 
+e classes adicionadas
+
+*/
+
+#include<iostream>
+
+// Inclusão do header config.cpp:
+#include "C:\Programacao\ProjetosGit\Trabalho-de-Programacao-Campo-Minado\source\config.hpp"
+
+int main(int argc, char *argv[]){
+    // (Teste)
+
+    return 0;
+}
