8e65766fd31e5ddf0e77a9797a43e770273b8346
eduardo-lamounier <a2025952788@teiacoltec.org>
2025-09-06 19:54:57 -0300

    Atualização do programa de teste das configurações: Testa agora as funções Get e InputSettings

--- tests/configTest.cpp
+++ tests/configTest.cpp
@@ -6,6 +6,7 @@ Data de criação: 28/08/2025
 Arquivo de teste para o header config.hpp, tem como objetivo testar as funções 
 e classes adicionadas
 
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 */
 
 #include<iostream>
@@ -13,17 +14,30 @@ e classes adicionadas
 // Inclusão do header config.cpp:
 #include "../include/config.hpp"
 
-using namespace std;
+void printConfigs(); // Imprime as configurações de GameConfig
+
+// ==================================< Main >==================================
 
 int main(int argc, char *argv[]){
     // Verificação da função Get() e dos valores inicialmente atribuídos às 
     // propriedades de GameConfig:
-    cout << "GameConfig::Width = " << GameConfig::Get("Width") << endl;
-    cout << "GameConfig::Height = " << GameConfig::Get("Height") << endl;
-    cout << "GameConfig::TotalBombs = " << GameConfig::Get("TotalBombs") << endl;
-    cout << "GameConfig::NoBombsRegion = " << GameConfig::Get("NoBombsRegion") << endl;
-
+    printConfigs();
     
+    // Testa a função de configuração:
+    GameConfig::InputSettings();
+
+    // Imprime as configurações depois da configuração feita pelo usuário
+    printConfigs();
 
     return 0;
+}
+
+/// @brief Imprime todas as propriedades de GameConfig
+void printConfigs(){
+    using namespace std;
+    
+    cout << "GameConfig::Width = " << GameConfig::Get("Width") << '\n';
+    cout << "GameConfig::Height = " << GameConfig::Get("Height") << '\n';
+    cout << "GameConfig::TotalBombs = " << GameConfig::Get("TotalBombs") << '\n';
+    cout << "GameConfig::NoBombsRegion = " << GameConfig::Get("NoBombsRegion") << '\n';
 }
