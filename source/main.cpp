2cfb1be68edb1e1580270c2ca93ff7c15065ce8e
eduardo-lamounier <a2025952788@teiacoltec.org>
2025-09-06 19:56:10 -0300

    Retirada da função menu (que vai ficar em outro header, não mais config.hpp)

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,7 @@
 /* 
 Arquivo que ao ser compilado, vai dar origem ao executável do jogo
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 */
 
 #include "..\include\config.hpp"
@@ -9,8 +11,8 @@ Arquivo que ao ser compilado, vai dar origem ao executável do jogo
 int main(int argc, char *argv[]){
     // (Talvez pegar dados do usuário como nickname e afins para colocar em
     // uma classe - apenas uma ideia)
-    menu(); // Cria o menu e pede pelas primeiras entradas do usuário
-    // (Chama a função para o usuário escolher as configurações do jogo)
+    // menu(); // Cria o menu e pede pelas primeiras entradas do usuário
+    // GameConfig::InputSettings() // (Chama a função para o usuário escolher as configurações do jogo)
 
     return 0;
 }
