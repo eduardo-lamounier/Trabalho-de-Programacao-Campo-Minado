145ab3d75ed8125912fc10ca99e1d0f214df6ae0
eduardo-lamounier <a2025952788@teiacoltec.org>
2025-09-13 14:51:07 -0300

    Adição de uma documentação no cabeçalho de main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,9 @@
 /* 
+
+Criado em: 27/08/2025 por Eduardo
+Responsabilidade de: Todos
+Finalizado em: --/--/2025
+
 Arquivo que ao ser compilado, vai dar origem ao executável do jogo
 
 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -12,7 +17,6 @@ int main(int argc, char *argv[]){
     // (Talvez pegar dados do usuário como nickname e afins para colocar em
     // uma classe - apenas uma ideia)
     // menu(); // Cria o menu e pede pelas primeiras entradas do usuário
-    // GameConfig::InputSettings() // (Chama a função para o usuário escolher as configurações do jogo)
 
     return 0;
 }
