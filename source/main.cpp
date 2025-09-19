c8907e76475378eba63f7c93c7576de613181d65
eduardo-lamounier <a2025952788@teiacoltec.org>
2025-09-19 15:30:00 -0300

    Melhora na documentação de source/main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -4,7 +4,8 @@ Criado em: 27/08/2025 por Eduardo
 Responsabilidade de: Todos
 Finalizado em: --/--/2025
 
-Arquivo que ao ser compilado, vai dar origem ao executável do jogo
+Arquivo que ao ser compilado, vai dar origem ao executável do jogo, além de
+ser o arquivo de implmentação do menu
 
 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 */
