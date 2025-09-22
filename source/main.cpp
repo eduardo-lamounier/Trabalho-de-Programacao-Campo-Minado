b270e1de061bd33063b5237446b7e395d4fd28fc
hollowmigs <a2025953148@teiacoltec.org>
2025-09-22 15:15:48 -0300

    wip: Faz a implementação inicial do menu

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,7 @@
 /* 
 
 Criado em: 27/08/2025 por Eduardo
-Responsabilidade de: Todos
+Responsabilidade de: Miguel
 Finalizado em: --/--/2025
 
 Arquivo que ao ser compilado, vai dar origem ao executável do jogo, além de
@@ -10,7 +10,39 @@ ser o arquivo de implmentação do menu
 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 */
 
-#include "..\include\config.hpp"
+#include "../include/config.hpp"
+
+void menu(char jogar_config_sair)
+
+int main(){ 
+
+;// auto config, auto sair)
+
+printf("\n\nCampo Minado\n\nJogo feito por Eduardo Lamounier, Heitor Freitas e Miguel Peterson\n.");
+
+printf("Digite:\n1.Jogar\n2.Configuracões\n3.Sair");
+scanf("%", &jogar_config_sair);
+
+switch (jogar_config_sair)
+{
+
+    case '1':
+    /* code */
+        break;
+    case '2':
+        break;
+    case '3':
+        printf("nao vou jogar campo minado nao, o silksong me espera");
+        return 0;
+        break;
+
+default:
+    break;
+    printf("1, 2 ou 3");
+}
+}
+
+
 
 // ==================================< main >==================================
 
