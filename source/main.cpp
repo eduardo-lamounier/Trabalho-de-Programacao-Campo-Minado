7913a76e8da0d301724e8fbad58646871c97a0b6
eduardo-lamounier <a2025952788@teiacoltec.org>
2025-09-25 15:55:53 -0300

    feat: Finalização da implementação do menu
    Melhora a interface e já finaliza totalmente a implementação do menu
    enquanto as ideas futuras não forem implentadas (e não precisam,
    necessariamente, ser). Melhora a entrada de dados e implementa o sistema
    de pontuação, mesmo que não seja ainda calculada pela função do jogo.

--- source/main.cpp
+++ source/main.cpp
@@ -2,7 +2,7 @@
 
 Criado em: 27/08/2025 por Eduardo
 Responsabilidade de: Miguel
-Finalizado em: --/--/2025
+Finalizado em: 25/09/2025
 
 Arquivo que ao ser compilado, vai dar origem ao executável do jogo, além de
 ser o arquivo de implmentação do menu
@@ -11,47 +11,63 @@ ser o arquivo de implmentação do menu
 */
 
 #include "../include/config.hpp"
+#include "../include/game.hpp"
 
-void menu(); // Protótipo da função dedicada à implementação do menu
+bool menu(int& points); // Protótipo da função dedicada à implementação do menu
 
 // ==================================< main >==================================
 
 int main(int argc, char *argv[]) {
-    // (Talvez pegar dados do usuário como nickname e afins para colocar em
-    // uma classe - apenas uma ideia)
-    // menu(); // Cria o menu e pede pelas primeiras entradas do usuário
+    int totalPoints = 0;
+    // TODO - só se sobrar tempo no planejamento:
+    // - Salvar as pontuações de cada usuário em um arquivo e implementar login
+    // e cadastro;
+    // - Fazer um top melhores jogadores (mas provavelmente não seria muito
+    // interessante porque só salvaria de maneira local)
+
+    while(!menu(totalPoints)); // Pede pelas primeiras entradas do usuário
+                               // enquanto ele não desejar finalizar o programa
+
+    std::cout << "Pontuacao total ganha durante tempo de execucao:\n";
+    std::cout << totalPoints << " Pontos.";
+    std::cout << "\n\nMuito obrigado por jogar!!" << std::endl;
 
     return 0;
 }
 
 // ==================================< menu >==================================
 
-void menu() {
-    int jogar_config_sair;
-    bool entradaValida;
+/// @brief Pede pela seleção do usuário para alguma opção do menu
+/// @param points Referência à pontuação atual do jogador, na qual as
+///               pontuações ganhas nos jogos vão ser somadas
+/// @return Se o programa deve finalizar ou não
+bool menu(int& points) {
+    char opcao;
 
-    std::cout << "\n\nCampo Minado\n\nJogo feito por Eduardo, Heitor e Miguel\n.";
+    std::cout << "\nBem-vindo ao Campo Minado\nJogo feito por: Eduardo, Heitor e Miguel\n\n";
+    std::cout << "Por favor, selecione uma das seguintes opções:\n";
+    std::cout << "1.Jogar\n2.Configuracoes\n3.Sair\n\n";
 
-    std::cout << "Digite:\n1.Jogar\n2.Configuracões\n3.Sair";
-    std::cin >> jogar_config_sair;
+    while(true) {
+        std::cout << "> " << std::endl;
+        std::cin >> opcao;
 
-    while(!entradaValida) {
-        entradaValida = jogar_config_sair >= 1 && jogar_config_sair <= 3;
-        switch (jogar_config_sair) {
+        switch (opcao) {
             case '1':
-                // Jogar
-                break;
+                // Jogar:
+                points += play();
+                return false; // Usuário não pretende finalizar o programa
             case '2':
-                // Configurar
-                break;
+                // Configurar:
+                GameConfig::InputSettings();
+                return false; // Usuário não pretende finalizar o programa
             case '3':
-                // Sair
-                std::cout << "\n<Programa finalizado, obrigado por jogar!\n";
-                break;
+                // Sair:
+                return true; // Usuário pretende finalizar o programa
             default:
-                // Usuário é um imbecil
-                break;
+                // Usuário é um imbecil (input inválido):
                 std::cout << "Digite uma opção válida: [1-3]\n";
+                break;
         }
     }
 }
