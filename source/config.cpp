d9e37e7fc14f5c0d441300a45c86fbbfc48d0a44
eduardo-lamounier <a2025952788@teiacoltec.org>
2025-11-04 09:30:07 -0300

    fix: Corrige função de leitura das configurações
    Anteriormente, a função de validação não era passada para a função de
    leitura, que por conta disso, acabava não conseguindo fazer a validação.
    Esse commit corrige isso incluindo a função de validação nas variáveis
    usadas para a leitura da função lambda 'input'.

--- source/config.cpp
+++ source/config.cpp
@@ -5,7 +5,7 @@ Responsabilidade de: Miguel
 Finalizado em: 25/09/2025
 
 # Visão geral:
-Arquivo .cpp complementar para implementar as funções, classes e 
+Arquivo .cpp complementar para implementar as funções, classes e
 estruturas e utilizar as contantes e variáveis globais antes protótipadas no
 header: "config.hpp"
 
@@ -44,19 +44,19 @@ int GameConfig::Get(std::string property) {
     // estavam em maiúsculo ou minúsculo):
     std::transform(property.begin(), property.end(), property.begin(), ::toupper);
 
-    if(property == "WIDTH") {
+    if (property == "WIDTH") {
         return GameConfig::Width;
     }
-    if(property == "HEIGHT") {
+    if (property == "HEIGHT") {
         return GameConfig::Height;
     }
-    if(property == "TOTALBOMBS") {
+    if (property == "TOTALBOMBS") {
         return GameConfig::TotalBombs;
     }
-    if(property == "NOBOMBSREGION") {
+    if (property == "NOBOMBSREGION") {
         return GameConfig::NoBombsRegion;
     }
-    
+
     std::string errorMessage = "Argumento invalido passado para a funcao: ";
     errorMessage += "int GameConfig::Get(std::string property). ";
     errorMessage += "Eh necessario passar uma string correspondente a alguma ";
@@ -67,101 +67,101 @@ int GameConfig::Get(std::string property) {
 /// @brief Faz a leitura das configurações escolhidas pelo usuário, e armazena
 ///        elas na classe GameConfig
 void GameConfig::InputSettings() {
-// Segunda versão da configuração - v.2
+    // Segunda versão da configuração - v.2
 
-// ------------------------------------------------------------------------
-    /// @brief Valida a leitura das configurações
-    /// @param configs Um conjunto de referências às variáveis que vão
-    ///                armazenar as configurações antes da confirmação pelo
-    ///                usuário
-    /// @param min O valor mínimo que essas configurações podem assumir
-    /// @param max O valor máximo que essas configurações podem assumir
-    constexpr auto validation = [] (std::vector<int*> configs, const int min,
-                                   const int max) {
-        
-        /// @brief Verifica se as configurações são válidas depois de repetir
-        ///        o input
-        /// @return Retorna se todas as configurações são válidas ou não
-        const auto validate = [&configs, min, max] () {
-            // Checa se alguma das configurações são inválidas:
-            for(auto config : configs) {
-                if(*config < min || *config > max) return false;
+    // ------------------------------------------------------------------------
+        /// @brief Valida a leitura das configurações
+        /// @param configs Um conjunto de referências às variáveis que vão
+        ///                armazenar as configurações antes da confirmação pelo
+        ///                usuário
+        /// @param min O valor mínimo que essas configurações podem assumir
+        /// @param max O valor máximo que essas configurações podem assumir
+    constexpr auto validation = [] (std::vector<int *> configs, const int min,
+        const int max) {
+
+            /// @brief Verifica se as configurações são válidas depois de repetir
+            ///        o input
+            /// @return Retorna se todas as configurações são válidas ou não
+            const auto validate = [&configs, min, max] () {
+                // Checa se alguma das configurações são inválidas:
+                for (auto config : configs) {
+                    if (*config < min || *config > max) return false;
+                }
+
+                // Se chegou até aqui, todas as configurações são válidas
+                return true;
+                };
+
+            while (!validate()) {
+                std::cout << "Digite valores validos";
+                std::cout << "[min: " << min << ", max:" << max << "]:\n";
+
+                std::cout << "> " << std::flush;
+                for (auto config : configs) std::cin >> *config;
+            }
+        };
+    // ------------------------------------------------------------------------
+        /// @brief Faz o input de um certo conjunto de configurações com as
+        ///        mesmas restrições enquanto pede para o usuário confirmá-las
+        /// @param inputText Texto do input dessas configurações
+        /// @param configs Um vetor de referências para as variáveis que vão
+        ///                armazenar essas configurações
+        /// @param configNames Um vetor com os nomes das configuraões, na mesma
+        ///                    ordem que 'configTest'
+        /// @param min O valor mínimo que essas configurações podem assumir
+        /// @param max O valor máximo que essas configurações podem assumir
+    auto input = [&validation](const std::string inputText,
+        std::vector<int *> configs,
+        const std::vector<std::string> configNames,
+        const int min, const int max) {
+
+            std::string errorMessage = "Valores inválidos passados para a função:";
+            errorMessage = "lambda input em: void GameConfig::InputSettings()";
+            if (configs.size() != configNames.size()) throw std::runtime_error(errorMessage);
+
+            char confirmation = 'N';
+
+            /// @brief Pede para o usuário confirmar ou não as configurações
+            ///        inseridas e atualiza 'confirmed'
+            const auto confirm = [&validation, &confirmation, &configs, &configNames] () {
+                std::cout << "Confirmar[S/N]?\n";
+
+                for (cmm::index i = 0; i < configs.size(); i++) {
+                    // Imprime o valor cada configuração inserida:
+                    std::cout << "|" << configNames[i] << " = ";
+                    std::cout << *configs[i] << "|\n";
+                }
+
+                std::cout << "> " << std::flush;
+                std::cin >> confirmation;
+                };
+
+            while (confirmation != 'S') {
+                std::cout << inputText;
+                std::cout << "[min: " << min << ", max: " << max << "]:\n";
+
+                std::cout << "> " << std::flush;
+                // Faz a leitura de cada configuração:
+                for (auto config : configs) std::cin >> *config;
+
+                // Validação da entrada:
+                validation(configs, min, max);
+
+                // Confirmação das configurações inseridas:
+                confirm();
             }
 
-            // Se chegou até aqui, todas as configurações são válidas
-            return true;
         };
-        
-        while(!validate()) {
-            std::cout << "Digite valores validos";
-            std::cout << "[min: " << min << ", max:" << max << "]:\n";
-            
-            std::cout << "> " << std::flush;
-            for(auto config : configs) std::cin >> *config;
-        }
-    };
-// ------------------------------------------------------------------------
-    /// @brief Faz o input de um certo conjunto de configurações com as
-    ///        mesmas restrições enquanto pede para o usuário confirmá-las
-    /// @param inputText Texto do input dessas configurações
-    /// @param configs Um vetor de referências para as variáveis que vão
-    ///                armazenar essas configurações
-    /// @param configNames Um vetor com os nomes das configuraões, na mesma
-    ///                    ordem que 'configTest'
-    /// @param min O valor mínimo que essas configurações podem assumir
-    /// @param max O valor máximo que essas configurações podem assumir
-    constexpr auto input = [] (const std::string inputText,
-                              std::vector<int*> configs,
-                              const std::vector<std::string> configNames,
-                              const int min, const int max) {
-
-        std::string errorMessage = "Valores inválidos passados para a função:";
-        errorMessage = "lambda input em: void GameConfig::InputSettings()";
-        if(configs.size() != configNames.size()) throw std::runtime_error(errorMessage);
-
-        char confirmation = 'N';
-
-        /// @brief Pede para o usuário confirmar ou não as configurações
-        ///        inseridas e atualiza 'confirmed'
-        const auto confirm = [&confirmation, &configs, &configNames] () {
-            std::cout << "Confirmar[S/N]?\n";
-
-            for(cmm::index i = 0; i < configs.size(); i++) {
-                // Imprime o valor cada configuração inserida:
-                std::cout << "|" << configNames[i] << " = ";
-                std::cout << *configs[i] << "|\n";
-            }
-
-            std::cout << "> " << std::flush;
-            std::cin >> confirmation;
-        };
-
-        while(confirmation != 'S') {
-            std::cout << inputText;
-            std::cout << "[min: " << min << ", max: " << max << "]:\n";
-
-            std::cout << "> " << std::flush;
-            // Faz a leitura de cada configuração:
-            for(auto config : configs) std::cin >> *config;
-
-            // Validação da entrada:
-            validation(configs, min, max);
-
-            // Confirmação das configurações inseridas:
-            confirm();
-        }
-        
-    };
-// ----------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------
 
     int width, height, totalBombs, noBombsRegion;
     std::string message; // Mensagem de input (atualiza para cada configuração)
-    
+
     // Entrada da altura e largura do campo minado:
     message = "Digite a altura e largura do campo, respectivamente";
-    
-    input(message, {&height, &width}, {"altura", "largura"},
-    MIN_WIDTH_ND_HEIGHT, MAX_WIDTH_ND_HEIGHT);
+
+    input(message, { &height, &width }, { "altura", "largura" },
+        MIN_WIDTH_ND_HEIGHT, MAX_WIDTH_ND_HEIGHT);
 
     // ------------------------------------------------------------------------
     const int area = width * height;
@@ -172,7 +172,7 @@ void GameConfig::InputSettings() {
     // Entrada do número de bombas:
     message = "Digite o numero total de bombas";
 
-    input(message, {&totalBombs}, {"total de bombas"}, minBombs, maxBombs);
+    input(message, { &totalBombs }, { "total de bombas" }, minBombs, maxBombs);
 
     // ------------------------------------------------------------------------
     const int maxNoBombsRegion = MAX_NO_BOMBS_REGION_PER_TOTAL_AREA * area;
@@ -181,8 +181,8 @@ void GameConfig::InputSettings() {
     message = "Digite o numero de quadrados reservados para ficarem";
     message += " sem bombas (regiao inicialmente revelada)";
 
-    input(message, {&noBombsRegion}, {"regiao sem bombas"},
-    MIN_NO_BOMBS_REGION, maxNoBombsRegion);
+    input(message, { &noBombsRegion }, { "regiao sem bombas" },
+        MIN_NO_BOMBS_REGION, maxNoBombsRegion);
 
     // ------------------------------------------------------------------------
 
